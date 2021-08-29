916bd7e6ce9b15f3c56eb5f05b98110f103145d4
Ondřej Piálek <ondrej@pialek.eu>
2021-08-29 17:16:30 +0200

    WIP data export

---
+++ src/TemataPodLupou.Web/ContentFinders/AlternativeTemplateContentFinder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Web.Routing;
+
+namespace TemataPodLupou.Web.ContentFinders
+{
+    public abstract class AlternativeTemplateContentFinder : ContentFinderByUrl
+    {
+        protected abstract string TemplateAlias { get; }
+        protected abstract string DocTypeAlias { get; }
+        protected abstract string UrlSlug { get; }
+
+        private readonly ITemplate _template;
+
+        public AlternativeTemplateContentFinder(ILogger logger, IFileService fileService)
+            : base(logger)
+        {
+            _template = fileService.GetTemplate(TemplateAlias);
+            if (_template == null)
+            {
+                Logger.Error<AlternativeTemplateContentFinder>("Cannot find template: {TemplateAlias}", TemplateAlias);
+            }
+        }
+
+        public override bool TryFindContent(PublishedRequest request)
+        {
+            var path = request.Uri.GetAbsolutePathDecoded();
+
+            if (request.HasDomain)
+                path = DomainUtilities.PathRelativeToDomain(request.Domain.Uri, path);
+
+            if (path == "/")
+            {
+                return false;
+            }
+
+            var lastSlashPosition = path.LastIndexOf('/');
+            var lastUrlSegment = path.Substring(lastSlashPosition + 1);
+
+            if (!string.Equals(lastUrlSegment, UrlSlug, StringComparison.InvariantCultureIgnoreCase))
+                return false;
+
+            var parentRoute = lastSlashPosition == 0 ? "/" : path.Substring(0, lastSlashPosition);
+            if (request.HasDomain)
+                parentRoute = request.Domain.ContentId + parentRoute;
+            var node = FindContent(request, parentRoute);
+
+            if (node == null)
+                return false;
+
+            if (node.ContentType.Alias != DocTypeAlias)
+            {
+                Logger.Debug<AlternativeTemplateContentFinder>(
+                    "Node at {Route} is not a {ExpectedDocType} but it is {ActualDocType}",
+                    parentRoute, DocTypeAlias, node.ContentType.Alias);
+                return false;
+            }
+
+            request.SetTemplate(_template);
+            return true;
+        }
+    }
+}
--- src/TemataPodLupou.Web/ContentFinders/SubmissionsCsvDownloadFinder.cs
+++ src/TemataPodLupou.Web/ContentFinders/SubmissionsCsvDownloadFinder.cs
@@ -8,22 +8,22 @@ using Umbraco.Web.Routing;
 
 namespace TemataPodLupou.Web.ContentFinders
 {
-    public class SubmissionWidgetHostScriptFinder : AlternativeTemplateContentFinder
+    public class SubmissionsCsvDownloadFinder : AlternativeTemplateContentFinder
     {
-        protected override string TemplateAlias => "SubmissionWidgetScriptEmbed";
+        protected override string TemplateAlias => "SubmissionWidgetDataExport";
         protected override string DocTypeAlias => SubmissionWidget.ModelTypeAlias;
-        protected override string UrlSlug => "embed";
+        protected override string UrlSlug => "export";
 
         [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
         public class Composer : IUserComposer
         {
             public void Compose(Composition composition)
             {
-                composition.ContentFinders().InsertBefore<ContentFinderByUrl, SubmissionWidgetHostScriptFinder>();
+                composition.ContentFinders().InsertBefore<ContentFinderByUrl, SubmissionsCsvDownloadFinder>();
             }
         }
 
-        public SubmissionWidgetHostScriptFinder(ILogger logger, IFileService fileService)
+        public SubmissionsCsvDownloadFinder(ILogger logger, IFileService fileService)
             : base(logger, fileService) { }
     }
 }
